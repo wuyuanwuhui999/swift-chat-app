25448a890da6a92a5e153263a3df9c6171cdbd76
wuwenqiang <275018723@qq.com>
2026-03-24 22:33:48 +0800

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by 吴文强 on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
