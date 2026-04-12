2f7b131f8b382d06719792e40e5ac14ef93ab245
wuwenqiang <275018723@qq.com>
2026-04-12 14:18:37 +0800

    修改提交脚本

---
+++ gitee-push.sh
@@ -0,0 +1,1 @@
+git remote rm origin; git remote add origin https://gitee.com/wuyuanwuhui99/swift-chat-app; git push -u origin main
---
+++ github-push.sh
@@ -0,0 +1,1 @@
+git remote rm origin; git remote add origin https://github.com/wuyuanwuhui999/swift-chat-app; git push -u origin main
